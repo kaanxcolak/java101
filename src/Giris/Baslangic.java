1ecc006a5cf0434bd1e2f204f40412ec370a62bf
Kaan Çolak <kaancolak3458@gmail.com>
2024-01-16 22:53:57 +0300

    Initial commit

---
+++ src/Giris/Baslangic.java
@@ -0,0 +1,11 @@
+package Giris;
+
+public class Baslangic {
+    public static void main(String[] args) {
+        System.out.printf("Merhaba Dünya !");
+        /**
+         *
+         * 
+         */
+    }
+}
